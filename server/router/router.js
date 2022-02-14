736170a31108d16babf6504ff5244f11a66d7730
Masoud <masoud_mdar@yahoo.fr>
2022-02-14 22:18:52 +0100

    testing

--- server/router/router.js
+++ server/router/router.js
@@ -7,7 +7,7 @@ module.exports = function(app, myDataBase) {
             res.send("Results table");
         })
 
-    app.route("/api/results/:player/:result")
+    app.route("/api/results/:player")
         .get((req, res) => {
             const {player} = req.params;
             console.log(player)
@@ -20,22 +20,23 @@ module.exports = function(app, myDataBase) {
 
         })
 
-        .post((req, res) => {
-            const {player} = req.params;
-            console.log(player)
-            const {result} = req.params;
-            console.log(result)
-            if (!player) {
-                throw new Error("Player not found");
-            }
+        // NOT WORKING FOR NOW
+        // .post((req, res) => {
+        //     const {player} = req.params;
+        //     console.log(player)
+        //     const {result} = req.params;
+        //     console.log(result)
+        //     if (!player) {
+        //         throw new Error("Player not found");
+        //     }
 
-            if (!result) {
-                throw new Error("No result sent");
-            }
+        //     if (!result) {
+        //         throw new Error("No result sent");
+        //     }
 
-            res.send("Add new result for the player")
+        //     res.send("Add new result for the player")
 
-        })
+        // })
         
 
 }
